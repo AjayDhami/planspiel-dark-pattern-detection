88445a3f002024b50e67b3759cc897eb8a3de92e
Prabudh Mishra <prabudh.mishra@s2022.tu-chemnitz.de>
2023-12-10 17:01:52 +0100

    SCRUM-22 Implemented routing scheme and added few pages to work around

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import ClientDashboardLayout from "./layouts/ClientDashboardLayout";
+import DashboardPage from "./pages/client/DashboardPage";
 
 function App() {
-  return <div className="App">V-Tenet Application</div>;
+  return (
+    <Routes>
+      {/* <!-- Common routes --> */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegistrationPage />} />
+
+      {/* <!-- Client Dashboard routes --> */}
+      <Route path="/client" element={<ClientDashboardLayout />}>
+        {/* Redirect to actual dashboard instead of just layout page */}
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
