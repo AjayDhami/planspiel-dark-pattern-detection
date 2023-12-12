c9715f6b9653efd55bb7d70274d2d282e647ad23
Kashfa Sehejat Sezuti <you@example.com>
2023-12-12 18:29:32 +0100

    updated landing signup signin pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import RegistrationPage from "./pages/RegistrationPage";
 import ClientDashboardLayout from "./layouts/ClientDashboardLayout";
 import DashboardPage from "./pages/client/DashboardPage";
+import SignIn from "./pages/SignInPage";
+import SignUp from "./pages/SignUpPage";
 
 function App() {
   return (
     <Routes>
       {/* <!-- Common routes --> */}
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegistrationPage />} />
-
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
       {/* <!-- Client Dashboard routes --> */}
       <Route path="/client" element={<ClientDashboardLayout />}>
         {/* Redirect to actual dashboard instead of just layout page */}
