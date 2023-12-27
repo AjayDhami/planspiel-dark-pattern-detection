c1da1e53f752a45f1f6c0a789984b5741bc86e2d
pdrashti8 <pdrashti1998@gmail.com>
2023-12-27 01:15:56 +0100

    VT-27 Test code for div text scraping

--- ml-model-service/web_scraping/scraping.py
+++ ml-model-service/web_scraping/scraping.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 driver = webdriver.Chrome()
-driver.get("https://www.cheapair.com/hotels/review?jsr=(bn:!n,corporateDiscountCode:%27%27,isPersonal:!f,nameNum:-1,occupantString:%272%27,paymentType:P,rateType:!n,rooms:!((arrives:%272024-01-02%27,chainCode:RT,departs:%272024-01-05%27,pricing:(currency:USD,rateUSD:85),propId:C9163,rateCode:%27240018861%27,rateKey:F~OjogZyQ0Nik2ZktnUVgdMGtXNzdjA2gLVlQNFAUFTghUc2wBBQtQTgRVFFQBLDBjaiFnbVcAVFACClsFAjwSVAxQAFdUUQQYDgYMVRkAUQFdHAxTAQJICgtWUVcBD1YIAwAGUTFUA1YADFYTMA0CWAkcNFW7NjhVoWI2BQNVAlKjNzwCEHFcEVVUA0J2BFSROE5iSkBMGEgEF1sQchMPXndQVkdKFxFdTwQTXBZ1QV4NEh1AE0MDDUVdFhwTDwJEbp9jNtY3OwpV_jg1Bl4EFwkBgjQ52TJnxDUx7DA19TFk-jBlxWcxy2EcVwNVUgUFDVYVAVVWBU4AXlAHS1UEAVBJVVFSUA1XUlUCUQBc1zd_A1EKVhoHChoAUzdTAQJRBV8CUxoKAlUFVQ8NDFRpimMzRQS8ZzLgzF2t,roomTypeCode:%27213001442%27,sourceSys:ER)),roomsId:0,searchId:300326429,shopRequest:(HSS:H,bn:!n,checkin:%272024-01-02%27,checkout:%272024-01-05%27,locId:CNCE,rms:%272%27))")
+driver.get("https://v-tenet.vercel.app/")
 
 driver.implicitly_wait(25)
 
@@ -14,14 +14,7 @@ div_texts = []
 
 # Fetching text from all div tags and sub div tags 
 body_divs = soup.body.find_all('div', recursive=True)  # Set recursive=False to only find direct children
-for div in body_divs:
-    # div_texts = list(div.text)
-    div_texts.append(div_texts)
-    list_items = div.find_all('li')
-    print(list_items)
-    if list_items:
-        for li in list_items:
-            div_texts.append(li)
+div_texts = [div.text for div in body_divs]
 
 
 # To filter empty list or lines from fetched data
