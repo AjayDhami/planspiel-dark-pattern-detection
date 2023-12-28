118620a1f1a245613e54a9711fd078ba5445e0b0
Drashti Patel <drashti.patel@s2022.tu-chemnitz.de>
2023-12-28 12:59:30 +0100

    Test code for data from all tags and subtags of div tag

--- ml-model-service/web_scraping/scraping.py
+++ ml-model-service/web_scraping/scraping.py
@@ -1,38 +1,52 @@
 from selenium import webdriver
-import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from ordered_set import OrderedSet
 
 
+# Initialize WebDriver
 driver = webdriver.Chrome()
 driver.get("https://v-tenet.vercel.app/")
 
-driver.implicitly_wait(25)
+# Use explicit wait for elements to be present in the DOM
+wait = WebDriverWait(driver, 25)
+wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'body')))
 
+# Fetch page source after the wait
 data = driver.page_source
 soup = BeautifulSoup(data, 'html.parser')
-div_texts = []
 
-# Fetching text from all div tags and sub div tags 
-body_divs = soup.body.find_all('div', recursive=True)  # Set recursive=False to only find direct children
-div_texts = [div.text for div in body_divs]
+# Find all div tags
+div_tags = soup.find_all('div')
 
+# Initialize list to store all text content 
+all_text = []
+
+# Traverse through all the div tags and iterate through nested tags inside the div
+for div_tag in div_tags:
+    for nested_tag in div_tag.find_all(recursive=False):
+        text = nested_tag.get_text(strip=True)
+        all_text.append(text)
 
 # To filter empty list or lines from fetched data
-filtered_list = [item for item in div_texts if item.strip() != '']
+filtered_list = [item for item in all_text if item.strip() != '']
 for i in range(len(filtered_list)):
     filtered_list[i] = filtered_list[i].replace('\n', '')
 
-# Remove duplicate elements
-filtered_list = list(set(filtered_list))
-# print(filtered_list)
+# Remove duplicate elements in order
+filtered_list = list(OrderedSet(filtered_list))
 
 # Store scraped data into a file
 output_file_path = 'output.txt'
-with open(output_file_path, 'w', encoding='utf-8') as file:
-    for text in filtered_list:
-        file.write(text + '\n')
-
-print(f'Data has been successfully written to {output_file_path}')
+try:
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        for text in filtered_list:
+            file.write(text + '\n')
+    print(f'Data has been successfully written to {output_file_path}')
+except Exception as e:
+    print(f'Error writing to the file: {e}')
 
 # Close browser window
 driver.quit()
