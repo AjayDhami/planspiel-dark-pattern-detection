bd920ce42c562c17e9b67ca98ab23f8e296850eb
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-01-13 01:11:45 +0530

    VT-48: Layout change for website dashboard

--- frontend/src/pages/expert/WebsiteDashboard.tsx
+++ frontend/src/pages/expert/WebsiteDashboard.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState, useContext, useCallback } from 'react'
 import { useExpertContext } from '../../context/ExpertContext'
-import { getPatternsData, getSpecificPattern, getSpecificWebsite } from '../../services/expertServices';
+import { getPatternsData, getSpecificPattern, getSpecificWebsite, stringAvatar } from '../../services/expertServices';
 import Navbar from '../../components/expert/Navbar';
 import PatternCard from '../../components/expert/PatternCard';
 import PatternAdditionForm from '../../components/expert/PatternAdditionForm';
 import PatternDetailsComponent from '../../components/expert/PatternDetailsComponent';
-import { PatternData, WebsiteData } from '../../types';
+import { PatternData } from '../../types';
 import { setRedirectCallback } from "../../utils/AxiosHelper";
 import AuthContext from "../../context/AuthContext1";
 import withExpertAuth from '../../hoc/withExpertAuth';
 import { toast } from "react-toastify";
-import { Tooltip} from '@mui/material';
+import { Avatar, Tooltip} from '@mui/material';
 
 
 const WebsiteDashboard = () => {
@@ -132,30 +132,42 @@ const WebsiteDashboard = () => {
     <>
         <Navbar/>
         <>
-        <div className='flex justify-between py-4 px-24'>
-          <div>
-            <h2 className='text-3xl font-bold text-blue-500'>{websiteName}</h2>
-          </div>
-          <div>
-            {websiteData.primaryExpertId === experId ? 
-              <Tooltip title={isPublishBtnDisabled ? "The patterns verification is incomplete" : "Publish all the patterns"} arrow>
-                <button className={`${bgForPublishBtn} p-2 rounded-lg`} disabled={isPublishBtnDisabled}>Publish</button>
-              </Tooltip> 
-            : null}
-          </div>
-        </div>
         <PatternAdditionForm isOpen={isPatternformOpen} onClose={closeFrom}/>
         <PatternDetailsComponent isOpen={isPatternModalOpen} onClose={closePatternModal} expertId={experId ? experId : ""}/>
-        <div className='mx-24 h-screen grid md:grid-cols-4 gap-4'>
-          <div className='md:col-span-1 shadow-xl rounded-2xl bg-white h-[30rem]'>
-            <div className='mx-4 mt-2 space-x-3 py-6 flex justify-center'>
-                <button onClick={openForm} className='px-8 py-2 rounded-md bg-blue-500 text-white'>Add a Pattern</button>
+        <div className='mx-24 h-screen grid md:grid-cols-3 gap-4 mt-8'>
+          <div className='md:col-span-1 shadow-xl rounded-2xl bg-white h-fit py-6 px-4'>
+            <div className='flex justify-between items-center'>
+              <h2 className='text-3xl font-bold text-blue-500'>{websiteName}</h2>
+              {websiteData.primaryExpertId === experId ? 
+                <Tooltip title={isPublishBtnDisabled ? "The patterns verification is incomplete" : "Publish all the patterns"} arrow>
+                  <button className={`${bgForPublishBtn} px-3 py-2 rounded-lg`} disabled={isPublishBtnDisabled}>Publish</button>
+                </Tooltip> 
+              : null}
             </div>
-            <div className='mx-8 mt-4 h-80 flex flex-col'>
+            <div className='text-lg px-2'>{websiteData.baseUrl}</div>
+            <div className='mt-3'>
+              <div className='w-full bg-gray-200 py-2 px-2 mt-2 rounded-lg'>
+                <h2 className='font-bold'>Description</h2>
+                <p>{websiteData.description}</p>
+              </div>
+              <div className='w-full rounded-lg mt-2 p-2'>
+                <h2 className='font-bold'>Experts</h2>
+                {/* {websiteData.expertIds.map((expert)=>(
+                  expert === websiteData.primaryExpertId ? <div className='font-italic'>{expert} - Primary</div> : <div className=' font-italic'>{expert}</div>
+                ))} */}
+                <div className='flex items-center my-2'><Avatar {...stringAvatar("Expert First")} className='mx-2'/>Expert First<span className='text-gray-400 italic'> - Primary</span></div>
+                <div className='flex items-center my-2'><Avatar {...stringAvatar("Expert Second")} className='mx-2'/>Expert Second</div>
+                <div className='flex items-center my-2'><Avatar {...stringAvatar("Expert Third")} className='mx-2'/>Expert Third</div>
+              </div>
+            </div>
+          </div>
+          <div className='md:col-span-2 h-full px-12 py-4 shadow-xl rounded-2xl bg-white'>
+              <div className='flex justify-between items-center mt-2'>
+                <div className='flex items-center'>
                 <div className='mx-2'>
                   <label htmlFor="patternlink" className='mb-2 block text-md font-medium'>Created By</label>
                   <select id="orient" 
-                    className='p-2 bg-transparent border-2 rounded-md w-60'
+                    className='p-2 bg-transparent border-2 rounded-md w-40'
                     onChange={(e) => handleSelectOption('expertName', e.target.value)}
                     >
                     <option value="">All</option>
@@ -164,10 +176,10 @@ const WebsiteDashboard = () => {
                     ))}
                   </select>
                 </div>
-                <div className='mx-2 mt-2'>
+                <div className='mx-2'>
                   <label htmlFor="patternlink" className='mb-2 block text-md font-medium'>Pattern Type</label>
                   <select id="orient" 
-                    className='p-2 bg-transparent border-2 rounded-md w-60'
+                    className='p-2 bg-transparent border-2 rounded-md w-40'
                     onChange={(e) => handleSelectOption('patternType', e.target.value)}
                     >
                     <option value="">All</option>
@@ -176,10 +188,10 @@ const WebsiteDashboard = () => {
                     ))}
                   </select>
                 </div>
-                <div className='mx-2 mt-2'>
+                <div className='mx-2'>
                   <label htmlFor="patternlink" className='mb-2 block text-md font-medium'>Status</label>
                   <select id="orient" 
-                    className='p-2 bg-transparent border-2 rounded-md w-60'
+                    className='p-2 bg-transparent border-2 rounded-md w-40'
                     onChange={(e) => handleSelectOption('phase', e.target.value)}
                     >
                     <option value="">All</option>
@@ -188,18 +200,20 @@ const WebsiteDashboard = () => {
                     ))}
                   </select>
                 </div>  
-            </div>
-          </div>
-          <div className='md:col-span-3 h-full px-12 py-4 shadow-xl rounded-2xl bg-white'>
-          {filteredArray.map((pattern,index)=>(
-            pattern.patternPhase === "InProgress" ? 
+              </div>
+              <div className='mx-2'>
+                <button onClick={openForm} className='px-8 py-2 rounded-md bg-blue-500 text-white'>Add a Pattern</button>
+              </div>
+              </div>
+            {filteredArray.map((pattern,index)=>(
+              pattern.patternPhase === "InProgress" ? 
               (<PatternCard patternKey={pattern.createdByExpertId} isAutoGenerated = {pattern.isAutoGenerated} loggedInExpert = {experId ? experId : ""} pattern_date={pattern.createdAt} pattern_type={pattern.patternType} expertName={pattern.expertName} color={"#F9C32F"} text={"In Progress"} hoverText = {"Awaiting Verification from experts"} openModal = {()=> openPatternModal(pattern.id,"#F9C32F")}/>)
               : pattern.patternPhase === "Verified" && pattern.isPatternExists === true ? 
               (<PatternCard patternKey={pattern.createdByExpertId} isAutoGenerated = {pattern.isAutoGenerated} loggedInExpert = {experId ? experId : ""} pattern_date={pattern.createdAt} pattern_type={pattern.patternType} expertName={pattern.expertName} color={"#E6321D"} text={"Verified"} hoverText = {"Verified but dark pattern exists"} openModal = {()=> openPatternModal(pattern.id,"#E6321D")}/>)
               : pattern.patternPhase === "Verified" && pattern.isPatternExists === false ? 
               (<PatternCard patternKey={pattern.createdByExpertId} isAutoGenerated = {pattern.isAutoGenerated} loggedInExpert = {experId ? experId : ""} pattern_date={pattern.createdAt} pattern_type={pattern.patternType} expertName={pattern.expertName} color={"#538D3F"} text={"Verified"} hoverText = {"Verified and dark pattern free"} openModal = {()=> openPatternModal(pattern.id,"#538D3F")}/>)
               : null
-          ))}
+            ))}
           </div>
         </div>
         </>
