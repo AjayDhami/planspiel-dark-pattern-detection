e89981d3d1ff954cfba545f1f30474f84d08faa1
Ajay Dhami <ajay.dhami@s2022.tu-chemnitz.de>
2024-01-23 16:57:14 +0100

    VT-45 unused and commented code removed from ml-model-service module

--- ml-model-service/dark_pattern_service.py
+++ ml-model-service/dark_pattern_service.py
@@ -1,6 +1,8 @@
-from model_training.dark_pattern_model_train import predict_website_dark_pattern_type, create_dark_pattern_detection_model
+from model_training.dark_pattern_model_train import predict_website_dark_pattern_type, \
+    create_dark_pattern_detection_model
 from model_training.scraping import web_scrap
 from flask import jsonify
+
 
 def create_model():
     print('Creating Dark Pattern Model')
@@ -12,14 +14,9 @@ def parse_website_url(website_id, params):
     print('Parsing website')
     website_url = params['websiteUrl']
 
-    # TODO change implementation of web_scrap method to accept list of website_urls and store their content in given website id
     web_scrap(website_url, website_id)
 
     dark_patterns = predict_website_dark_pattern_type(website_id)
     dark_patterns = [{'text': key, 'patternType': value} for key, value in dark_patterns.items()]
 
-    # for text, pattern_type in dark_patterns.items():
-    #     print(f"{text}: {pattern_type}")
-
     return jsonify(dark_patterns)
-    # return 'Successfully website parsed', 200
