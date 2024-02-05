ae630e555ac049db2b0ef3bbbe0204c7793e210f
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-02-06 01:12:29 +0530

    VT-68: zIndex bug resolved in image carausel

--- frontend/src/widget.js
+++
@@ -1,9 +0,0 @@
-const widgetContainer = document.getElementById('widget-container');
-
-// Create your widget's HTML structure here
-const widgetElement = document.createElement('div');
-widgetElement.classList.add('widget');
-widgetElement.textContent = 'This is my widget!';
-
-// Append the widget element to the container
-widgetContainer.appendChild(widgetElement);
