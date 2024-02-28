da6c0138239430c670edec5f544b361266bfd9c4
Amay Rajvaidya <amay.rajvaidya@s2022.tu-chemnitz.de>
2024-02-29 02:12:04 +0530

    VT-68: Data getter setup for extension

--- widget/popup.js
+++ widget/popup.js
@@ -29,3 +29,44 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
     });
 });
+
+const getData = async () => {
+    const key = 'patternType'
+    let patterns = await new Promise((resolve, reject) => {
+        chrome.storage.local.get(key, (result) => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } 
+            const patterns = result.patternType ?? [];
+            resolve(patterns)
+        });
+    });
+    const updatedPatterns = patterns || [];
+    console.log(patterns);
+    const patternList = document.getElementById("patternobj-list");
+    if(Array.isArray(imageData)){
+        updatedPatterns.forEach(element => {
+            const listItem = document.createElement("div");
+            listItem.className = "patternItemsDiv";
+            // listItem.textContent = `Type: ${element.patternType}, description: ${element.patternDesc}`
+            listItem.innerHTML = `
+                <div style="display:flex; align-items:center; color:azure;">
+                    <h3>${element.patternType}</h3>
+                    <i class="material-icons patternRemove" data-pattern-time="${element.patternTime}" style="cursor:pointer;">delete</i>
+                </div>
+                <p style="color:azure;">${element.patternDesc}</p>
+            `;
+            element.patternimages.forEach(image=>{
+                const imgItem = document.createElement("div");
+                imgItem.innerHTML = `
+                    <div style="margin: 2px;">
+                        <img src="data:image/png;base64,${image.file_base64}" width="200" />
+                    </div>
+                `
+                listItem.appendChild(imgItem);
+            })
+            patternList.appendChild(listItem)
+        });
+    }
+}
+
